e72661944761cf51ecede9d5d83a54f97fee42ba
Brian <wamwangimathenge@gmail.com>
2023-09-18 11:38:50 +0300

    Create a simple bio script
    This commit creates variable which are used to generate a simple bio
    when the script is executed.

---
+++ profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name="Brian"
+age="24"
+school="Azubi Africa"
+course="Cloud Engineering"
+career="DevOps Cloud Engineer"
+
+echo "Hello, my name is $name and I am $age years old."
+echo "I am currently pursuing $course at $school."
+echo "My career aspirations is to be a $career"
+echo "Thank you"
