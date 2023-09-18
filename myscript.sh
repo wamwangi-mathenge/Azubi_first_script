0837880f6519c96e25038138989d3668a18ff6fb
Brian <wamwangimathenge@gmail.com>
2023-09-18 11:13:47 +0300

    Assign a variable for and use it in the output
    This commit creates a variable which stores a name.
    The name variable is then invoked thereby displaying the name
    assigned to the variable.

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "Hello World"
-echo "This is my first script"
-echo "These are the contents of this directory":
+name="Brian"
+echo "Hello $name"
+echo "This is your first script"
+echo "These are the contents of this directory"
 ls
 echo "Thank you and have a great day"
